eed614bb0af0e6085995ee11f12b5f017264cd16
Rida R. <58844180+rida-rech@users.noreply.github.com>
2025-10-21 17:31:47 +0200

    chnage

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -8,16 +8,18 @@ terraform {
 }
 
 provider "aws" {
-  access_key                  = "test"
-  secret_key                  = "test"
-  region                      = "us-east-1"
-  s3_force_path_style         = true
+  access_key = "test"
+  secret_key = "test"
+  region     = "us-east-1"
+
   skip_credentials_validation = true
-  skip_metadata_api_check     = true
   skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
+
   endpoints {
     s3       = "http://localhost:4566"
     sqs      = "http://localhost:4566"
     dynamodb = "http://localhost:4566"
+    sts      = "http://localhost:4566"
   }
 }
